34ec2ca046bf8cc5f8b8e6c68c5fb106cbfef242
FaithD186 <faith.dong@mail.utoronto.ca>
2022-12-09 11:17:28 -0500

    startup

--- PF/PB/startup.sh
+++ PF/PB/startup.sh
@@ -6,4 +6,10 @@ source venv/bin/activate
 pip3 install -r requirements.txt
 python3 manage.py makemigrations
 python3 manage.py migrate
+cd ..
+cd project_frontend
+npm install
+npm install --save google-maps-react --legacy-peer-deps
+cd ..
+cd PB
 
